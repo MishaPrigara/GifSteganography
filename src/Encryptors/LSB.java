3793dc245deb6b64673690f62d34a315ab0aeb24
MishaPrigara <mishaprigaraz@gmail.com>
2019-02-28 00:06:47 +0200

    lsb refactor

--- src/Encryptors/LSB.java
+++ src/Encryptors/LSB.java
@@ -2,10 +2,29 @@ package Encryptors;
 
 import Helpers.Binary;
 import Helpers.EncryptException;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 
 public class LSB {
+
+    private byte[] bytes;
+
+    private void put(@NotNull byte[] curArray, int fpos) {
+        for(int i = 0; i < curArray.length / 2; i++) {
+            if(curArray[i * 2] == 1) {
+                bytes[fpos + i] |= (1 << 7);
+            } else {
+                bytes[fpos + i] &= ~(1 << 7);
+            }
+            if(curArray[i * 2 + 1] == 1) {
+                bytes[fpos + i] |= (1 << 6);
+            } else {
+                bytes[fpos + i] &= ~(1 << 6);
+            }
+        }
+    }
+
 
     public void Encrypt(File in, File out, String text, byte key1, long key2) throws IOException, EncryptException {
         if(!in.isFile()) {
@@ -18,7 +37,7 @@ public class LSB {
             throw new IOException("No input text");
         }
 
-        byte[] bytes = new byte[(int)in.length()];
+        bytes = new byte[(int)in.length()];
         InputStream is = new FileInputStream(in);
         is.read(bytes);
         is.close();
@@ -40,42 +59,23 @@ public class LSB {
         int curByte = 13;
 
         byte[] curArray = Binary.toBitArray(key1);
-        for(int i = 0; i < curArray.length / 2; i++) {
-            if(curArray[i * 2] == 1) {
-                bytes[curByte] |= (1 << 7);
-            } else {
-                bytes[curByte] &= ~(1 << 7);
-            }
-            if(curArray[i * 2 + 1] == 1) {
-                bytes[curByte] |= (1 << 6);
-            } else {
-                bytes[curByte] &= ~(1 << 6);
-            }
-            curByte++;
-        }
+        put(curArray, curByte);
+
+        curByte += curArray.length / 2;
+
+
 
         byte[] textBytes = text.getBytes();
 
         for(int i = 0; i < textBytes.length; i++) {
             curArray = Binary.toBitArray(textBytes[i]);
-            for(int j = 0; j < curArray.length / 2; j++) {
-                if(curArray[i * 2] == 1) {
-                    bytes[curByte] |= (1 << 7);
-                } else {
-                    bytes[curByte] &= ~(1 << 7);
-                }
-                if(curArray[i * 2 + 1] == 1) {
-                    bytes[curByte] |= (1 << 6);
-                } else {
-                    bytes[curByte] &= ~(1 << 6);
-                }
-                curByte++;
-            }
+            put(curArray, curByte);
+            curByte += curArray.length / 2;
         }
 
         maxLength *= 4;
 
-        for(; 13 + curByte <= maxLength; curByte++) {
+        for(; curByte - 13 < maxLength; curByte++) {
             bytes[curByte++] &= ~(1 << 7);
             bytes[curByte++] &= ~(1 << 7);
         }
