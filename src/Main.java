27d062bf27e2864e7f539e40d01705f9fd159fab
MishaPrigara <mishaprigaraz@gmail.com>
2019-02-27 22:46:58 +0200

    added try/catch in main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import Encryptors.LSB;
+import Helpers.EncryptException;
 
 import java.io.File;
 import java.io.IOException;
@@ -7,9 +8,15 @@ public class Main {
 
     static LSB lsb = new LSB();
 
-    public static void main(String[] args) throws IOException {
-        lsb.Encrypt(new File("/home/mishaprigara/Downloads/earth.gif"),
-                new File("/home/mishaprigara/Downloads/earth.gif"), "kek");
+    public static void main(String[] args) throws IOException, EncryptException {
+        try {
+            lsb.Encrypt(new File("/home/mishaprigara/Downloads/earth.gif"),
+                    new File("/home/mishaprigara/Downloads/earth.gif"), "kek", (byte)0, 0);
+        } catch (EncryptException err) {
+            System.out.println("Encryption failed: " + err);
+        } catch (IOException err) {
+            System.out.println("Something's wrong with input: " + err);
+        }
         System.out.println("kek");
     }
 }
